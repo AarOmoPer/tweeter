04cacd8b4dc99b8c03f292728c109087754c93a4
Aaron Omotosho <aaronomotosho@gmail.com>
2018-10-19 16:20:38 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { getNewTweetAndTweeters } from '../data'
+
+class App extends React.Component {
+  state = {
+    streak: 0,
+    tweet: {},
+    tweeters: [],
+    restart: false 
+  }
+
+  componentDidMount() {
+    this.setState({
+      ...getNewTweetAndTweeters()
+    })
+  }
+
+  render() {
+    const { streak, tweet, tweeters, restart} = this.state;
+    return (
+      <section>
+        <h2 className='main-header'>Tweeter</h2>
+
+        {!restart 
+          ? <section>
+              <h3>Your score: {streak}</h3>
+
+              <p className='tweet-container' style={{ border: "1px solid black", padding: "10px" }}>{tweet.body}</p>
+
+              <p className='helper-container'>Who tweeted it?</p>
+
+              <section className='tweeter-container'>
+                {
+                  tweeters.map((tweeter, ind) =>
+                    <button
+                      key={ind}
+                      onClick={e => this.handleAnswerSubmission(e, tweeter.handle)}
+                      className='tweeter-button'
+                    >
+                      {tweeter.author}
+                    </button>)
+                }
+              </section>
+            </section>
+          : <section>
+              <h2>Whoops!</h2>
+              <p>The right answer was {tweet.author}</p>
+              <p>Your streak was {streak}</p>
+              <button>Share on twitter</button>
+              <button onClick={this.resetStreak}>Restart</button>
+            </section>
+        }
+
+      </section>
+    );
+  }
+
+  handleAnswerSubmission = (e, tweeterHandle) => {
+    e.preventDefault()
+    const { streak, tweet } = this.state;
+    if (tweeterHandle === tweet.handle) {
+      this.setState({ streak: streak + 1 ,  ...getNewTweetAndTweeters()})
+    } else {
+      this.setState({restart: true})
+    }
+  }
+
+  resetStreak = () => {
+    this.setState({ restart: false, streak: 0, ...getNewTweetAndTweeters() })
+  }
+
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,78 @@
+const tweets = {
+  0: {
+    body: "Fake news",
+    author: "Donald Trump",
+    handle: "@realDonaldTrump",
+    likes: 2000,
+    retweets: 500
+  },
+  1: {
+    body: "Bootilicious",
+    author: "Kim Kardashian",
+    handle: "@kimK",
+    likes: 2004,
+    retweets: 900
+  },
+  2: {
+    body: "Danger Zone!",
+    author: "Sterling Archer",
+    handle: "@dutchess",
+    likes: 3009,
+    retweets: 9000
+  },
+  3: {
+    body: "Bet ya a signed dollar it's true",
+    author: "Elon Musk",
+    handle: "@muskyBoi",
+    likes: 1982,
+    retweets: 6000
+  },
+  4: {
+    body: "I'm a dark skinned white man as far as I know.",
+    author: "Kanye West",
+    handle: "@Yeezy",
+    likes: 369,
+    retweets: 900
+  }
+}
+
+const getNewTweetAndTweeters = () => {
+  const randomInt = Math.floor(Math.random() * 5)
+  const tweet = tweets[randomInt]
+  return {
+    tweet,
+    tweeters: getNewTweeters({author: tweet.author, handle: tweet.handle})
+  }
+}
+
+const getNewTweeters = tweeter => {
+  const tweeters = [tweeter];
+  while (tweeters.length < 4){
+    const randomInt = Math.floor(Math.random() * 5)
+    const existingHandles = tweeters.map(tweeter => tweeter.handle);
+    const nextTweeter = tweets[randomInt]
+    if (!existingHandles.includes(nextTweeter.handle)){
+      tweeters.push({
+        author: nextTweeter.author,
+        handle: nextTweeter.handle
+      })
+    }
+  }
+  return shuffleArray(tweeters)
+}
+
+const shuffleArray = a => {
+  let j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
+
+
+export {
+  getNewTweetAndTweeters
+}
