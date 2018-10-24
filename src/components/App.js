4226a030a7eca6cf079095bb66938dbe19dcb26d
Aaron Omotosho <aaronomotosho@gmail.com>
2018-10-24 20:49:28 +0100

    refactored to new backend data format

--- src/components/App.js
+++ src/components/App.js
@@ -1,76 +1,93 @@
 import React from 'react';
-import { getNewTweetAndTweeters } from '../data'
 
-import {getUserTweets} from '../controller'
-getUserTweets('@realDonaldTrump')
+// getUserTweets('@realDonaldTrump')
 
 class App extends React.Component {
   state = {
     streak: 0,
     tweet: {},
     tweeters: [],
-    restart: false 
+    timerRemaining: 10,
+    restart: false
   }
 
   componentDidMount() {
-    this.setState({
-      ...getNewTweetAndTweeters()
-    })
-    // getUserTweets('@realDonaldTrump')
+    this.getNewTweet()
   }
 
   render() {
-    const { streak, tweet, tweeters, restart} = this.state;
+    const { streak, tweet, tweeters, restart, timerRemaining} = this.state;
     return (
       <section>
         <h2 className='main-header'>Tweeter</h2>
-
-        {!restart 
+        {!restart
           ? <section>
-              <h3>Your score: {streak}</h3>
+            <h3>Your streak: {streak}</h3>
+            <h3>Seconds left: {timerRemaining}</h3>
 
-              <p className='tweet-container' style={{ border: "1px solid black", padding: "10px" }}>{tweet.body}</p>
+            <p className='tweet-container' style={{ border: "1px solid black", padding: "10px" }}>{tweet.text}</p>
 
-              <p className='helper-container'>Who tweeted it?</p>
+            <p className='helper-container'>Who tweeted it?</p>
 
-              <section className='tweeter-container'>
-                {
-                  tweeters.map((tweeter, ind) =>
+            <section className='tweeter-container'>
+              {
+                tweeters.map((tweeterData, ind) => {
+                  const tweeter = tweeterData.data[0]
+                  return (
                     <button
                       key={ind}
-                      onClick={e => this.handleAnswerSubmission(e, tweeter.handle)}
-                      className='tweeter-button'
-                    >
-                      {tweeter.author}
-                    </button>)
-                }
-              </section>
+                      onClick={e => this.handleAnswerSubmission(e, tweeter.screen_name)}
+                      className='tweeter-button'>
+                      {tweeter.name}
+                    </button>
+                  )
+                })
+              }
             </section>
+          </section>
           : <section>
-              <h2>Whoops!</h2>
-              <p>The right answer was {tweet.author}</p>
-              <p>Your streak was {streak}</p>
-              <button>Share on twitter</button>
-              <button onClick={this.resetStreak}>Restart</button>
-            </section>
+            <h2>Whoops!</h2>
+            <p>The right answer was {tweet.user.name}</p>
+            <p>See the tweet <a href={`http://www.twitter.com/*/status/${tweet.id_str}`}>here</a></p>
+            <p>Your streak was {streak}</p>
+            <button>Share on twitter</button>
+            <button onClick={this.resetStreak}>Restart</button>
+          </section>
         }
 
       </section>
     );
   }
 
+  resetTimer = () => {
+    clearInterval(this.state.timer)
+    this.setState({timerRemaining: 10})
+    const timer = this.state.timer
+    timer()
+  }
+  tick = () => this.setState({timer: this.state.timer - 1})
+  
+  getNewTweet = () => fetch('https://tweeter-backend.herokuapp.com')
+    .then(res => res.json())
+    .then(res => this.setState({ ...res }))
+    //Start timer
+    // .then(() )
+    .catch(console.log)
+
   handleAnswerSubmission = (e, tweeterHandle) => {
     e.preventDefault()
-    const { streak, tweet } = this.state;
-    if (tweeterHandle === tweet.handle) {
-      this.setState({ streak: streak + 1 ,  ...getNewTweetAndTweeters()})
+    const {streak, tweet} = this.state;
+    if (tweeterHandle === tweet.user.screen_name) {
+      this.setState({ streak: streak + 1, tweet: {}, tweeters: [] })
+      this.getNewTweet()
     } else {
-      this.setState({restart: true})
+      this.setState({ restart: true })
     }
   }
 
   resetStreak = () => {
-    this.setState({ restart: false, streak: 0, ...getNewTweetAndTweeters() })
+    this.setState({ restart: false, streak: 0, tweet: {}, tweeters: [] })
+    this.getNewTweet()
   }
 
 }
